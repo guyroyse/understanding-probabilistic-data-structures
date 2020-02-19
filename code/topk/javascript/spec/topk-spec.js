887702990987fbc3ed065395c4137a9daa1f7d06
Guy Royse <guy@guyroyse.com>
2020-02-19 17:10:03 -0500

    now with a min heap

--- code/topk/javascript/spec/topk-spec.js
+++ code/topk/javascript/spec/topk-spec.js
@@ -8,37 +8,95 @@ describe("TopK", function() {
   let subject
 
   beforeEach(function() {
-    subject = new TopK(4, 100, 1.05)
+    subject = new TopK({ k: 3, depth: 4, width: 100, decay: 1.05 })
   })
 
-  it("returns a count of 0 when queried for unknown string", function() {
-    expect(subject.query("foo")).to.equal(0)
+  describe("#query", function() {
+
+    it("returns a count of 0 when queried for unknown string", function() {
+      expect(subject.query("foo")).to.equal(0)
+    })
+  
+    it("returns a count of 1 for a single item", function() {
+      subject.add("foo")
+      expect(subject.query("foo")).to.equal(1)
+    })
+  
+    it("returns a bigger count for multiple items", function() {
+      subject.add("foo")
+      subject.add("foo")
+      subject.add("foo")
+      expect(subject.query("foo")).to.equal(3)
+    })
+  
+    it("multiple strings can be counted", function() {
+      subject.add("foo")
+      subject.add("foo")
+      subject.add("foo")
+      subject.add("bar")
+      subject.add("bar")
+      subject.add("baz")
+  
+      expect(subject.query("foo")).to.equal(3)
+      expect(subject.query("bar")).to.equal(2)
+      expect(subject.query("baz")).to.equal(1)
+      expect(subject.query("qux")).to.equal(0)
+    })
+  
   })
 
-  it("returns a count of 1 for a single item", function() {
-    subject.add("foo")
-    expect(subject.query("foo")).to.equal(1)
+  describe("#top", function() {
+
+    it("returns an empty array for an empty TopK", function() {
+      expect(subject.top())
+        .to.be.an('array')
+        .that.is.empty
   })
 
-  it("returns a bigger count for multiple items", function() {
-    subject.add("foo")
-    subject.add("foo")
-    subject.add("foo")
-    expect(subject.query("foo")).to.equal(3)
+    it("returns the count of a single item", function() {
+      subject.add("foo")
+      subject.add("foo")
+      subject.add("foo")
+      expect(subject.top())
+        .to.be.an('array')
+        .that.has.lengthOf(1)
+        .that.has.deep.ordered.members([{ s: 'foo', count: 3 }]);
+    })
+
+    it("returns an ordered count of multiple items", function() {
+      subject.add("foo")
+      subject.add("foo")
+      subject.add("foo")
+      subject.add("bar")
+      subject.add("bar")
+      subject.add("baz")
+      expect(subject.top())
+        .to.be.an('array')
+        .that.has.lengthOf(3)
+        .that.has.deep.ordered.members([
+          { s: 'baz', count: 1 },
+          { s: 'bar', count: 2 },
+          { s: 'foo', count: 3 }]);
+    })
+
+    it("doesn't count more than k items", function() {
+      subject.add("foo")
+      subject.add("foo")
+      subject.add("foo")
+      subject.add("foo")
+      subject.add("bar")
+      subject.add("bar")
+      subject.add("bar")
+      subject.add("baz")
+      subject.add("baz")
+      subject.add("qux")
+      expect(subject.top())
+        .to.be.an('array')
+        .that.has.lengthOf(3)
+        .that.has.deep.ordered.members([
+          { s: 'baz', count: 2 },
+          { s: 'bar', count: 3 },
+          { s: 'foo', count: 4 }]);
+    })
   })
-
-  it("multiple strings can be counted", function() {
-    subject.add("foo")
-    subject.add("foo")
-    subject.add("foo")
-    subject.add("bar")
-    subject.add("bar")
-    subject.add("baz")
-
-    expect(subject.query("foo")).to.equal(3)
-    expect(subject.query("bar")).to.equal(2)
-    expect(subject.query("baz")).to.equal(1)
-    expect(subject.query("qux")).to.equal(0)
-  })
-
 })
--- code/topk/javascript/topk.js
+++ code/topk/javascript/topk.js
@@ -1,6 +1,8 @@
 const murmur = require('murmurhash')
 const Heap = require('heap')
 
+let compareCount = (a, b) => a.count - b.count
+
 class HashArray {
   constructor(width) {
     let seed = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)
@@ -53,25 +55,45 @@ class Bucket {
   }
 
   decay(rate) {
-    let probability = Math.pow(rate, bucket.counter * -1)
+    let probability = Math.pow(rate, this.counter * -1)
 
     if (Math.random(1) <= probability) {
-      bucket.decrement()
+      this.decrement()
     }
   }
 }
 
 class TopK {
-  constructor(depth, width, decay) {
-
+  constructor({ k, depth, width, decay }) {
+    this.k = k
     this.decay = decay
+    this.minHeap = new Heap(compareCount)
     this.hashArrays = new Array(depth)
       .fill().map(_ => new HashArray(width))
-
-    this.minHeap = new Heap((a, b) => a.counter - b.counter)
   }
 
   add(s) {
+    this.addToHeavyKeeper(s)
+    this.addToMinHeap(s)
+  }
+
+  query(s) {
+    return this.hashArrays
+      .map(hashArray => {
+        let fingerprint = hashArray.getFingerprintOf(s)
+        let bucket = hashArray.getBucketForFingerprint(fingerprint)
+        return bucket.hasFingerprint(fingerprint) ? bucket : null
+      })
+      .filter(bucket => bucket !== null)
+      .map(bucket => bucket.counter)
+      .reduce((previous, current) => Math.max(previous, current), 0)
+  }
+
+  top() {
+    return this.minHeap.toArray().sort(compareCount)
+  }
+
+  addToHeavyKeeper(s) {
 
     this.hashArrays.forEach(hashArray => {
 
@@ -96,18 +118,30 @@ class TopK {
       }
 
     })
+
   }
 
-  query(s) {
-    return this.hashArrays
-      .map(hashArray => {
-        let fingerprint = hashArray.getFingerprintOf(s)
-        let bucket = hashArray.getBucketForFingerprint(fingerprint)
-        return bucket.hasFingerprint(fingerprint) ? bucket : null
-      })
-      .filter(bucket => bucket !== null)
-      .map(bucket => bucket.counter)
-      .reduce((previous, current) => Math.max(previous, current), 0)
+  addToMinHeap(s) {
+
+    let count = this.query(s)
+    let item = this.minHeap.toArray().find(item => item.s === s)
+
+    if (item) {
+      item.count = count
+      this.minHeap.heapify()
+      return
+    }
+    
+    if (this.minHeap.size() < this.k) {
+      this.minHeap.push({ s, count })
+      return
+    }
+    
+    if (count > this.minHeap.peek().count) {
+      this.minHeap.replace({ s, count })
+      return
+    }
+
   }
 
 }
